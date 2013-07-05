38e4818182c1ada5bea72c244eb115f0e20bc2f6
liu tao <star2ko@gmail.com>
2013-07-05 18:49:56 +0800

    db update

--- db/ddl.sql
+++ db/ddl.sql
@@ -1,16 +1,16 @@
 CREATE TABLE `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(45) NOT NULL,
-  `email` varchar(20) NOT NULL,
+  `email` varchar(20) DEFAULT NULL,
   `type` tinyint(4) NOT NULL DEFAULT '0',
   `status` tinyint(4) NOT NULL DEFAULT '0',
-  `password` varchar(45) NOT NULL,
+  `password` varchar(100) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   PRIMARY KEY (`id`),
   KEY `idx_email` (`email`),
   KEY `idx_name` (`name`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 
 CREATE TABLE `teachers` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -25,4 +25,3 @@ CREATE TABLE `teachers` (
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
-
